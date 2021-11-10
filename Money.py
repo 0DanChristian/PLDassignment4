f295f9e702658e44faefcb0213570cc346aaa695
Dan Christian <dansilog16@gmail.com>
2021-11-10 14:02:12 -0800

    Money.py

---
+++ Money.py
@@ -0,0 +1,10 @@
+# assignment 4
+# do everything you did w your recent assignment
+# but this time, w only one function
+
+def hm_money():
+    mmnt_money = int(input("Please enter the how much money you have: "))
+    ppl_prc = int(input("Please enter the amount of apple you want to buy: "))
+    return mmnt_money, ppl_prc
+
+
