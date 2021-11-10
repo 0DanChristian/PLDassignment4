727fb2ba6e4282c6c88f240d1ac1af6e7e684bf7
Dan Christian <dansilog16@gmail.com>
2021-11-10 14:14:48 -0800

    AppleOrange.py

---
+++ AppleOrange.py
@@ -0,0 +1,12 @@
+def pricelist():
+    pple_price = 20
+    rng_price = 25
+    print(f"Our orange costs {rng_price} pesos and for our apple, it will only cost you {pple_price} pesos.")
+
+def hmany():
+    ppl_hmany = int(input("How many apples do you want? "))
+    rng_hmany = int(input("How many orange do you want? "))
+    return ppl_hmany, rng_hmany
+
+
+    
